3f8eb7d3e62db77ab328310773d15f675c073fb1
Mona <shamona9594@gmail.com>
2019-09-21 21:49:58 +0430

    initialize the project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const firebase = require('firebase');
+require('@firebase/firestore');
+
+firebase.initializeApp({
+    //Here you have to define the following data accordin to your firebase project
+    apiKey: "---",
+    authDomain: "---",
+    databaseURL: "---",
+    projectId: "---",
+    storageBucket: "---",
+    messagingSenderId: "---",
+    appId: "---"
+});
+
+ReactDOM.render(<App />, document.getElementById('evernote-container'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
